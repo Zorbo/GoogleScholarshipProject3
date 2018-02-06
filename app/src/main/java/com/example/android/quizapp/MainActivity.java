b40dc1aa47fd48a78243ec205f1fc2aaf43fb4c5
Zorbo <tamas.kiss.job@gmail.com>
2018-02-06 17:50:24 +0100

    second commit

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.android.quizapp;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private int score = 0;
+    private String playerName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,35 +22,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private CharSequence getCustomerName(){
-
-        TextView playerName = findViewById(R.id.input_name);
-        return playerName.toString();
-
-    }
-
     public void displayResult(View view){
 
-        if (score > 0) {
-            Toast.makeText(getApplicationContext(),"HOHOHOHHOOHOHHOH",Toast.LENGTH_LONG).show();
+
+        if (score == 0) {
+            Toast.makeText(getApplicationContext(), "Dear " + playerName + " you scored: " + score +
+                    " out of 6\n NEVER GIVE UP!", Toast.LENGTH_LONG).show();
             return;
-        } else {
-            score++;
         }
 
+        if (score > 0 && score <= 3) {
+            Toast.makeText(getApplicationContext(), "Dear " + playerName + " you scored: " + score +
+                    " out of 6\n KEEP PRACTICING!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (score > 3) {
+            Toast.makeText(getApplicationContext(), "Dear " + playerName + " you scored: " + score +
+                    " out of 6\n CONGRATULATIONS!", Toast.LENGTH_LONG).show();
+        }
     }
 
 
     public void clickNext(View view) {
+
+        EditText name = findViewById(R.id.name_field);
+        playerName = name.getText().toString();
+
         setContentView(R.layout.page_one);
 
     }
 
     public void clickNextPageOne(View view) {
 
-        RadioButton buttonPageOne = findViewById(R.id.page1_radio_button1);
+        RadioButton buttonPageOne = findViewById(R.id.page1_radio_button2);
         if (buttonPageOne.isChecked()) {
-            score++;
+            score = score + 1;
         }
 
         setContentView(R.layout.page_two);
@@ -57,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
         RadioButton buttonPageOne = findViewById(R.id.page2_radio_button3);
         if (buttonPageOne.isChecked()) {
-            score++;
+            score = score + 1;
         }
 
         setContentView(R.layout.page_three);
@@ -67,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
         RadioButton buttonPageOne = findViewById(R.id.page3_radio_button1);
         if (buttonPageOne.isChecked()) {
-            score++;
+            score = score + 1;
         }
 
         setContentView(R.layout.page_four);
@@ -75,9 +85,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickNextPageFour(View view) {
 
-        RadioButton buttonPageOne = findViewById(R.id.page4_radio_button2);
+        RadioButton buttonPageOne = findViewById(R.id.page4_radio_button3);
         if (buttonPageOne.isChecked()) {
-            score++;
+            score = score + 1;
         }
 
         setContentView(R.layout.page_five);
@@ -87,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
         RadioButton buttonPageOne = findViewById(R.id.page5_radio_button1);
         if (buttonPageOne.isChecked()) {
-            score++;
+            score = score + 1;
         }
 
         setContentView(R.layout.page_six);
@@ -97,31 +107,10 @@ public class MainActivity extends AppCompatActivity {
 
         RadioButton buttonPageOne = findViewById(R.id.page6_radio_button3);
         if (buttonPageOne.isChecked()) {
-            score++;
+            score = score + 1;
         }
 
         setContentView(R.layout.page_end);
     }
-
-    public void clickBackPageTwo(View view) {
-        setContentView(R.layout.page_one);
-    }
-
-    public void clickBackPageThree(View view) {
-        setContentView(R.layout.page_two);
-    }
-
-    public void clickBackPageFour(View view) {
-        setContentView(R.layout.page_three);
-    }
-
-    public void clickBackPageFive(View view) {
-        setContentView(R.layout.page_four);
-    }
-
-    public void clickBackPageSix(View view) {
-        setContentView(R.layout.page_five);
-    }
-
 
 }
